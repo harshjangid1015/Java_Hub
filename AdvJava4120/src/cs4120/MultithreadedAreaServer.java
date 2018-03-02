5f89caa04d05cd5d6456aa6910e8280d9a04a513
Harsh Jangid <harshjangid1015@gmail.com>
2018-03-02 16:56:24 -0600

    MultithreadedAreaServer.java

--- AdvJava4120/src/cs4120/MultithreadedAreaServer.java
+++ AdvJava4120/src/cs4120/MultithreadedAreaServer.java
@@ -1,5 +1,62 @@
 package cs4120;
 
-public class MultithreadedAreaServer {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
 
+public class MultithreadedAreaServer {
+	public static void main(String[] args) {
+		try {
+			int clientID = 1;
+			int port = 8000;
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.out.println("Server started");
+			
+			while(true) {
+				Socket socket  = serverSocket.accept();
+				
+				System.out.println("Client " +clientID+ " connected");
+				HandleClientTask task = new HandleClientTask(socket, clientID);
+				
+				Thread thread = new Thread(task);
+				thread.start();
+				clientID++;
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+}
+class HandleClientTask implements Runnable{
+	private Socket socket;
+	private int id;
+	
+	HandleClientTask(Socket socket, int id){
+		this.socket = socket;
+		this.id = id;
+	}
+
+	@Override
+	public void run() {
+		try {
+			DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+			DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+			
+			while(true) {
+				double radius = inputFromClient.readDouble();
+				
+				System.out.println("Received radius " + radius + "from client " + this.id);
+				
+				double area = radius*radius*Math.PI;
+				
+				outputToClient.writeDouble(area);
+				
+				System.out.println("sent area "+ area + " to clent " + this.id);
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		
+	}
+	
 }
