67d79d7ab14fc9c29c27674be99ff1de9c42bb32
Harsh Jangid <harshjangid1015@gmail.com>
2018-03-12 12:22:09 -0500

    Gomoku modified

--- AdvJava4120/src/cs4120/Gomoku.java
+++ AdvJava4120/src/cs4120/Gomoku.java
@@ -11,11 +11,16 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 
 public class Gomoku extends Application {
+	
+	char whoseTurn = 'X';
 
-	private Cell[][] cell =  new Cell[19][19];
+	Cell[][] cell =  new Cell[19][19];
+	
+	Label lblStatus = new Label("X's turn to play");
 
 	@Override
 	public void start(Stage primaryStage){
@@ -26,11 +31,24 @@ public class Gomoku extends Application {
 				gridPane.add(cell[i][j] = new Cell(), j, i);
 			}
 		}
+		
+		Circle A = new Circle();
+		A.setRadius(15);
+		A.setStroke(Color.BLACK);
+		A.setFill(Color.BLACK);
+//		gridPane.getChildren().add(A);
+		
+		Circle B = new Circle();
+		B.setRadius(15);
+		B.setStroke(Color.BLACK);
+		B.setFill(Color.WHITE);
+//		gridPane.getChildren().add(B);
 
 
 		BorderPane borderPane = new BorderPane();
 		borderPane.setStyle("-fx-border-color: red");
 		borderPane.setCenter(gridPane);
+		borderPane.setBottom(lblStatus);
 		BackgroundFill fill = new BackgroundFill(Color.BEIGE, null, null);
 		Background background = new Background(fill);
 		gridPane.setBackground(background);
@@ -38,7 +56,6 @@ public class Gomoku extends Application {
 
 		Scene scene = new Scene(borderPane);
 		primaryStage.setTitle("Gomoku");
-
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
@@ -56,5 +73,10 @@ class Cell extends Pane{
 	public Cell() {
 		setStyle("-fx-border-color: black");
 		this.setPrefSize(50, 50);
+		this.setOnMouseClicked(e -> handleMouseClick());
+	}
+	
+	public void handleMouseClick(){
+		
 	}
 }
