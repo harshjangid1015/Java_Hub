9cbf0ee02719af9bc2c37276af6043792270a593
Harsh Jangid <harshjangid1015@gmail.com>
2018-03-27 22:51:34 -0500

    ChatRoom code now can communicate

--- AdvJava4120/src/cs4120/Q3_ChatRoomClient.java
+++ AdvJava4120/src/cs4120/Q3_ChatRoomClient.java
@@ -10,98 +10,75 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Q3_ChatRoomClient extends Application {
+	ObjectOutputStream outputToServer = null;
+	ObjectInputStream  inputFromServer = null;
+	private TextArea ta = new TextArea();
 
 	@Override
 	public void start(Stage primaryStage) {
 		BorderPane borderPane = new BorderPane();
-		
+		ScrollPane scrollPane = new ScrollPane(ta);
+
 		TextField inputTextField = new TextField();
-		
+
 		Button send = new Button("SEND");
 		HBox hBox = new HBox();
 		hBox.getChildren().add(inputTextField);
 		hBox.getChildren().add(send);
-		
-		
+
 		borderPane.setBottom(hBox);
+		borderPane.setCenter(scrollPane);
 		Scene scene = new Scene(borderPane, 400, 600);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("ChatRoomClientr");
+		primaryStage.setTitle("ChatRoomClient");
 		primaryStage.show();
+;
+		ta.appendText("clent started" + '\n');;
+		try {
+			Socket socket = new Socket("localhost", 8000);
+			ta.appendText("clent connected to server" + '\n');
+
+			 outputToServer = new ObjectOutputStream(socket.getOutputStream());
+			  inputFromServer = new ObjectInputStream(socket.getInputStream());
+		}catch(Exception ex) {
+
+		}
+		new Thread(()->{
+		
+//		inputTextField.setOnAction(e -> {
+			try {
+				while(true) {
+					String message;
+					send.setOnAction(event -> {
+						try {
+							String message1 = inputTextField.getText();
+							outputToServer.writeObject(message1);
+							outputToServer.flush();
+							inputTextField.clear();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+					});
+					message = (String)inputFromServer.readObject();
+					ta.appendText("Server: "+message + '\n');
+				}
+			}catch(Exception ex) {
+
+			}
+//		});
+		}).start();
+
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-//	public static void main(String[] args) {
-//		
-//		try {
-//			Socket socket = new Socket("localhost", 8000);
-//			System.out.println("client connected to server");
-//			
-//			ReadFromServerThread readTask = new ReadFromServerThread(socket);
-//			WriteToServerThread writeTask = new WriteToServerThread(socket);
-//			
-//			Thread thread1 = new Thread(readTask);
-//			Thread thread2 = new Thread(writeTask);
-//			
-//			thread1.start();
-//			thread2.start();
-//			
-//		}catch (Exception ex) {
-//			System.out.println(ex);
-//		}
-//	}
-}
-
-class ReadFromServerThread implements Runnable{
-	private Socket socket;
-	public ReadFromServerThread(Socket socket) {
-		this.socket = socket;
-	}
-	@Override
-	public void run() {
-		try {
-			ObjectInputStream inputFromServer = new ObjectInputStream(socket.getInputStream());
-			while(true) {
-				String message = (String)inputFromServer.readObject();
-				System.out.println("Server: " + message);
-			}
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		
-	}
-	
-}
-
-class WriteToServerThread implements Runnable{
-	private Socket socket;
-	public WriteToServerThread(Socket socket) {
-		this.socket = socket;
-	}
-	@Override
-	public void run() {
-		try {
-			ObjectOutputStream outputToServer = new ObjectOutputStream(socket.getOutputStream());
-			Scanner input = new Scanner(System.in);
-			while(true) {
-				String message = input.nextLine();
-//				System.out.println("Client: " + message);
-				outputToServer.writeObject(message);
-				
-			}
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		
-	}
-	
 }
--- AdvJava4120/src/cs4120/Q3_ChatRoomServer.java
+++ AdvJava4120/src/cs4120/Q3_ChatRoomServer.java
@@ -1,128 +1,90 @@
 package cs4120;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.Date;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class Q3_ChatRoomServer extends Application{
+public class Q3_ChatRoomServer extends Application {
+	// Text area for displaying contents
+	private TextArea ta = new TextArea();
 
-	@Override
+	// Number a client
+
+	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
 		BorderPane borderPane = new BorderPane();
-		
+		ScrollPane scrollPane = new ScrollPane(ta);
+
+
 		TextField inputTextField = new TextField();
-		
 		Button send = new Button("SEND");
 		HBox hBox = new HBox();
-		hBox.getChildren().add(inputTextField);
-		hBox.getChildren().add(send);
-		
-		
-		borderPane.setBottom(hBox);
+		hBox.getChildren().addAll(inputTextField, send);
+		hBox.setAlignment(Pos.CENTER);
+
+		// Create a scene and place it in the stage
 		Scene scene = new Scene(borderPane, 400, 600);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("ChatRoomServer");
-		primaryStage.show();
+		borderPane.setBottom(hBox);
+		borderPane.setCenter(scrollPane);
+		primaryStage.setTitle("MultiThreadServer"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+
+		new Thread( () -> {
+			try {
+				// Create a server socket
+				ServerSocket serverSocket = new ServerSocket(8000);
+				Platform.runLater(() ->
+				ta.appendText("Server started at " + new Date() + '\n'));
+
+				// Listen for a connection request
+				Socket socket = serverSocket.accept();
+
+				// Create data input and output streams
+				ObjectInputStream  inputFromClient = new ObjectInputStream(socket.getInputStream());
+				ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());
+				while (true) {
+					
+					String	message = (String)inputFromClient.readObject();
+					ta.appendText("Client: "+message + '\n');
+					
+					send.setOnAction(event -> {						
+						try {							
+							String	message2 = inputTextField.getText();
+							outputToClient.writeObject(message2);
+							outputToClient.flush();
+							inputTextField.clear();
+							
+						} catch (IOException e) {
+							e.printStackTrace();
+						}					
+				});
+				}				
+			}
+			catch(IOException ex) {
+				ex.printStackTrace();
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+			}
+		}).start();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-//	public static void main(String[] args) {
-//		try {
-//			int clientID = 1;
-//			int port = 8000;
-//			ServerSocket serverSocket = new ServerSocket(port);
-//			System.out.println("Server started");
-//
-//			while(true) {
-//				Socket socket  = serverSocket.accept();
-//				System.out.println("Client " +clientID+ " connected");
-//
-//				ReadFromClientThread readTask = new ReadFromClientThread(socket, clientID);
-//				WriteToServertThread writeTask = new WriteToServertThread(socket, clientID);
-//				//				HandleClientTask task = new HandleClientTask(socket, clientID);
-//
-//				Thread thread1 = new Thread(readTask);
-//				Thread thread2 = new Thread(writeTask);
-//				thread1.start();
-//				thread2.start();
-//				clientID++;
-//			}
-//		} catch (Exception ex) {
-//			System.out.println(ex);
-//		}
-//	}
-
 }
-class ReadFromClientThread implements Runnable{
-	private Socket socket;
-	private int id;
 
-	ReadFromClientThread(Socket socket, int id){
-		this.socket = socket;
-		this.id = id;
-	}
 
-	@Override
-	public void run() {
-		try {
-			ObjectInputStream inputFromClient = new ObjectInputStream(socket.getInputStream());
-			//			ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());
 
-			while(true) {
-
-				String message = (String) inputFromClient.readObject();
-				System.out.println("Client: " + message);
-				//				double radius = inputFromClient.readDouble();
-				//				
-				//				System.out.println("Received radius " + radius + "from client " + this.id);
-				//				
-				//				double area = radius*radius*Math.PI;
-				//				
-				//				outputToClient.writeDouble(area);
-				//				
-				//				System.out.println("sent area "+ area + " to clent " + this.id);
-			}
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}	
-	}	
-}
-class WriteToServertThread implements Runnable{
-	private Socket socket;
-	private int id;
-
-	WriteToServertThread(Socket socket, int id){
-		this.socket = socket;
-		this.id = id;
-	}
-
-	@Override
-	public void run() {
-		try {
-			ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());
-			Scanner input = new Scanner(System.in);
-			while(true) {
-				//				String message = input.next();
-				String message = input.nextLine();
-				//				System.out.println("Server: " + message);
-				outputToClient.writeObject(message);
-
-			}
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}	
-	}	
-}
