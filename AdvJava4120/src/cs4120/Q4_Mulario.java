41e270e3188e4a739e92f660817500de28b58d4e
Harsh Jangid <harshjangid1015@gmail.com>
2018-04-02 19:41:39 -0500

    Mulario game complete

--- AdvJava4120/src/cs4120/Q4_Mulario.java
+++ AdvJava4120/src/cs4120/Q4_Mulario.java
@@ -2,23 +2,24 @@ package cs4120;
 
 import java.util.ArrayList;
 
-import javafx.animation.PathTransition;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 /**
  * 2. (50 points) Q4_Mulario
@@ -32,7 +33,6 @@ set the color of their circle by specifying red, green, and blue values, each in
 circle.*/
 
 public class Q4_Mulario extends Application{
-	Label lblStatus = new Label("");
 	private ArrayList<Circle> nodes = new ArrayList<>();
 	Pane pane = new Pane();
 	Circle c = new Circle();
@@ -42,6 +42,8 @@ public class Q4_Mulario extends Application{
 	HBox hBox = new HBox();
 	Button colButton = new Button("set color");
 	CheckBox cb = new CheckBox("Display Radius");
+	Text text = new Text("User");
+	
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -49,64 +51,65 @@ public class Q4_Mulario extends Application{
 		borderPane.setStyle("-fx-border-color: blue");
 //		StackPane stackPane = new StackPane();
 //		stackPane.setStyle("-fx-border-color: green");
-//		Pane pane = new Pane();
 		pane.setStyle("-fx-border-color: red");
 //		stackPane.getChildren().add(pane);
-		
-		
-		
+		text.setFont(new Font(20));
+
 		borderPane.setTop(hBox);
 		borderPane.setCenter(pane);
-//		borderPane.setBottom(lblStatus);
-		
-		
-//		hBox.getChildren().add(tf);
-		
-//		hBox.getChildren().add(colButton);
-//		RadioButton rb = new RadioButton("Display Radius");
-		
-		
+
 		cb.setOnAction(e -> {
 			if(cb.isSelected()) {
-				hBox.getChildren().add(lblStatus);
+				pane.getChildren().add(text);				
 			}else {
-				hBox.getChildren().remove(lblStatus);
+				pane.getChildren().remove(text);
 			}
 		});
+
 		colButton.setOnAction(ev -> {
 			int r = Integer.parseInt(tfr.getText());
 			int g = Integer.parseInt(tfg.getText());
 			int b = Integer.parseInt(tfb.getText());
-			//c.setFill(Color.rgb(Double.parseDouble(tfr.getText()), Double.parseDouble(tfg.getText()), Double.parseDouble(tfb.getText()));
 			c.setFill(Color.rgb(r, g, b));
+			tfr.clear();
+			tfg.clear();
+			tfb.clear();
 			pane.requestFocus();
 		});
+
+//		hBox.getChildren().addAll(tfr,tfg,tfb, colButton, cb);
+		;
+		hBox.getChildren().add(new Label("r"));
+		hBox.getChildren().add(tfr);
+		hBox.getChildren().add(new Label("g"));
+		hBox.getChildren().add(tfg);
+		hBox.getChildren().add(new Label("b"));
+		hBox.getChildren().add(tfb);		
+		hBox.getChildren().add(colButton);
+		hBox.getChildren().add(cb);
 		
-				
-		hBox.getChildren().addAll(tfr,tfg,tfb, colButton, cb);
-		
-		
+		hBox.setAlignment(Pos.CENTER);
+
 		for(int i=0; i<20;i++) {
 			Circle cir = new Circle();
 			cir.setRadius(10+(Math.random()*40));
-			cir.setCenterX(50+Math.random()*1150);
-			cir.setCenterY(50+Math.random()*850);
+			cir.setCenterX(50+Math.random()*1100);
+			cir.setCenterY(50+Math.random()*800);
 			cir.setStroke(Color.RED);
 			cir.setFill(Color.color(Math.random(), Math.random(), Math.random()));
 			nodes.add(cir);
 			pane.getChildren().add(cir);			
 		}
-				
 
-//		Circle c = new Circle();
 		c.setRadius(50);
-		c.setCenterX(50+Math.random()*1150);
-		c.setCenterY(50+Math.random()*850);
+		c.setCenterX(50+Math.random()*1100);
+		c.setCenterY(50+Math.random()*800);
 		c.setStroke(Color.BLACK);
 		c.setFill(Color.GREEN);
-		
 		pane.getChildren().add(c);
-				
+		text.setX(c.getCenterX());
+		text.setY(c.getCenterY());
+
 		pane.setOnKeyPressed(e -> keyHandler(e, c));
 
 		Scene scene = new Scene(borderPane, 1200, 900);
@@ -114,7 +117,7 @@ public class Q4_Mulario extends Application{
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		pane.requestFocus();
-	
+
 		tfr.setFocusTraversable(false);
 		tfg.setFocusTraversable(false);
 		tfb.setFocusTraversable(false);
@@ -122,61 +125,52 @@ public class Q4_Mulario extends Application{
 		colButton.setFocusTraversable(false);
 		hBox.setFocusTraversable(false);
 	}
-	
-	
+
 	public void keyHandler(KeyEvent e, Circle c) {
 		switch(e.getCode()) {
 		case UP:
-//			pane.setStyle("-fx-border-color: red");
-			pane.requestFocus();
-			tfr.setFocusTraversable(false);
-			tfg.setFocusTraversable(false);
-			tfb.setFocusTraversable(false);
-			cb.setFocusTraversable(false);
-			colButton.setFocusTraversable(false);
-			hBox.setFocusTraversable(false);
 			c.setCenterX(c.getCenterX());
 			c.setCenterY(c.getCenterY()-10);
+			text.setX(c.getCenterX());
+			text.setY(c.getCenterY());
 			checkCircleIntersection(c);
-			
 			break;
 		case DOWN:
 			c.setCenterX(c.getCenterX());
 			c.setCenterY(c.getCenterY()+10);
+			text.setX(c.getCenterX());
+			text.setY(c.getCenterY());
 			checkCircleIntersection(c);
 			break;
 		case LEFT:
 			c.setCenterX(c.getCenterX()-10);
 			c.setCenterY(c.getCenterY());
+			text.setX(c.getCenterX());
+			text.setY(c.getCenterY());
 			checkCircleIntersection(c);
 			break;
 		case RIGHT:
 			c.setCenterX(c.getCenterX()+10);
 			c.setCenterY(c.getCenterY());
+			text.setX(c.getCenterX());
+			text.setY(c.getCenterY());
 			checkCircleIntersection(c);
 			break;
 		default:
 			break;
-
 		}
-		
 	}
-	
+
 	public void eatCircle(Circle c, Circle cir) {		
 		Circle mainCircle = (Circle)c;
 		Circle otherCircle = (Circle)cir;
 		mainCircle.setRadius(mainCircle.getRadius() + 5);
 		double newRadius = mainCircle.getRadius()+5;
-		lblStatus.setText(String.format(" %02X", (int)newRadius));
-//		lblStatus = "mainCircle.setRadius(mainCircle.getRadius() + 5)";
-//		Platform.runLater(() ->{
-//			nodes.remove(otherCircle);
-//			pane.getChildren().remove(otherCircle);
-//		});
+		text.setText(Double.toString(newRadius));
 		nodes.remove(otherCircle);
 		pane.getChildren().remove(otherCircle);
 	}
-	
+
 	public void checkCircleIntersection(Circle chk) {
 		boolean collisionDetected = false;
 		for (Circle static_circle : nodes) {
@@ -191,7 +185,6 @@ public class Q4_Mulario extends Application{
 			}			
 		}
 	}
-
 
 	public static void main(String[] args) {
 		launch(args);
