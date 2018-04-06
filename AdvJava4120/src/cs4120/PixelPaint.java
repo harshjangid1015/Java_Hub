d999a4b094b73986906638575ea2a480a7aa8cd5
Harsh Jangid <harshjangid1015@gmail.com>
2018-04-05 22:47:49 -0500

    Pixel Paint final save & load functionality added

--- AdvJava4120/src/cs4120/PixelPaint.java
+++ AdvJava4120/src/cs4120/PixelPaint.java
@@ -1,179 +1,168 @@
 package cs4120;
 
-import javafx.scene.control.TextArea;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.input.MouseEvent;
-//import javafx.scene.control.Cell;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
 
 public class PixelPaint extends Application {
 	
-//	char whichColor = 'G';
-	Color whichColor;
-	
-	Cell[][] cell = new Cell[32][32];
-	TextArea ta = new TextArea();
-	Label lblStatus = new Label("select a color from ColorPicker");
 	ColorPicker colorPicker = new ColorPicker();
-	
+	Cell[][] cell = new Cell[32][32];
+	Color whichColor;
+
 	@Override
 	public void start(Stage primaryStage) {
-		
 		GridPane gridPane = new GridPane();
+		BorderPane borderPane  = new BorderPane();
+		borderPane.setStyle("-fx-border-color: red");
+		gridPane.setStyle("-fx-border-color: green");
+		HBox hBox = new HBox();
+		borderPane.setTop(hBox);
+		borderPane.setCenter(gridPane);
+		Button SAVE = new Button("SAVE");
+		Button LOAD = new Button("LOAD IMAGE");
+		hBox.getChildren().add(colorPicker);
+		hBox.getChildren().add(SAVE);
+		hBox.getChildren().add(LOAD);
+		
 		for(int i=0; i<32; i++) {
 			for (int j=0; j<32; j++) {
 				gridPane.add(cell[i][j] = new Cell(), j, i);
 			}
 		}
-//		ColorPicker colorPicker = new ColorPicker();
-//		colorPicker.setValue(Color.RED);
-//		Button RED = new Button("RED");
-//		Button GREEN = new Button("GREEN");
-//		Button BLUE = new Button("BLUE");
-		Button SAVE = new Button("SAVE");
-		HBox hBox = new HBox();
-//		HBox hBox2 = new HBox();
-//		hBox.getChildren().add(RED);
-//		hBox.getChildren().add(GREEN);
-//		hBox.getChildren().add(BLUE);
 		
-		hBox.getChildren().add(colorPicker);
-		hBox.getChildren().add(SAVE);
-//		hBox.getChildren().add(ta);
-		hBox.getChildren().add(lblStatus);
 		colorPicker.setOnAction(e -> {
 			whichColor = colorPicker.getValue();
-//			lblStatus.setText(colorPicker.getValue().toString());
 		});
-//		RED.setOnAction(e -> {
-//			whichColor = 'R';
-//			lblStatus.setText("Red Brush Selected");});
-//		GREEN.setOnAction(e -> {
-//			whichColor = 'G';
-//			lblStatus.setText("GREEN Brush Selected");});
-//		BLUE.setOnAction(e -> {
-//			whichColor = 'B';
-//			lblStatus.setText("BLUE Brush Selected");});
+		
 		SAVE.setOnAction(e -> {
-			for(int i=0; i<32; i++) {
-				for (int j=0; j<32; j++) {
-					
-					
+			File aFile = new File("newFile.txt");
+			try {
+				PrintWriter writer = new PrintWriter(aFile);
+				for(int i=0; i<32; i++) {
+					for (int j=0; j<32; j++) {					
+							if(cell[i][j].getBackground() != null) {
+								Background bb = cell[i][j].getBackground();
+								BackgroundFill fill2 = bb.getFills().get(0);
+								Paint p = fill2.getFill();
+								Color d = (Color)p;
+								int red = (int) (d.getRed()*255);
+								int green = (int) (d.getGreen()*255);
+								int blue = (int) (d.getBlue()*255);
+//								System.out.println(red + "\t" + green + "\t" + blue);
+								writer.println(red + "\t" + green + "\t" + blue);
+							}					
+					}
 				}
+
+				writer.close();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		});
 		
-		
-		BorderPane borderPane  = new BorderPane();
-		borderPane.setStyle("-fx-border-color: red");
-		borderPane.setTop(hBox);
-		
-//		borderPane.setBottom(hBox2);
-		borderPane.setCenter(gridPane);
-//		borderPane.setBottom(lblStatus);
+		LOAD.setOnAction(e -> {
+			File aFile = new File("newFile.txt");
+			Color col;
+			try {
+				Scanner reader = new Scanner(aFile);
+				//int i = 0 ; int j=0;
+				while(reader.hasNext()) {
+//					int red = reader.nextInt();
+//					int green = reader.nextInt();
+//					int blue = reader.nextInt();
+//					System.out.println(red + "\t" + green + "\t" + blue);
+					
+					
+//					col = Color.rgb(red, green, blue);
+//					BackgroundFill fill = new BackgroundFill(col, null, null);
+//					Background background = new Background(fill);
+//					cell[i][j].setBackground(background);
+//					i++;
+//					if(i%32 ==0)
+//					{
+//						j++;
+//						i = 0;
+//					}
+					
+					for(int i=0; i<32; i++) {
+						for (int j=0; j<32; j++) {
+							int red = reader.nextInt();
+							int green = reader.nextInt();
+							int blue = reader.nextInt();
+							col = Color.rgb(red, green, blue);
+							BackgroundFill fill = new BackgroundFill(col, null, null);
+							Background background = new Background(fill);
+							cell[i][j].setBackground(background);
+						}
+					}
+						
+				}
+				
+				
+				reader.close();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
 		
 		Scene scene = new Scene(borderPane);
 		primaryStage.setTitle("Pixel Paint");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
 	}
 	
-	
 	class Cell extends StackPane{
-		private Color brush;
-				
+		
 		public Cell() {
 			setStyle("-fx-border-color: black");
 			this.setPrefSize(25, 25);
+			BackgroundFill fill = new BackgroundFill(Color.WHITE, null, null);
+			Background background = new Background(fill);
+			this.setBackground(background);
 			this.setOnMouseClicked(e -> handleMouseClick());
 		}
 		
-		public Color getBrush() {
-			return brush;
+		public Color getColor() {
+			return whichColor;
 		}
-
-		/** Set a new token */
-		public void setBrush(Color c) {
-			brush = c;
-			Rectangle r =new Rectangle();
-			r.setWidth(23);
-			r.setHeight(23);
-			r.setFill(brush);
-			getChildren().add(r);
-			
-			
-			lblStatus.setText(String.format("r%02X g%02X b%02X", (int)(brush.getRed()*255), (int)(brush.getGreen()*255), (int)(brush.getBlue()*255)));
-			
-//			lblStatus.setText(String.format(Integer.toHexString(brush.getBlue().hashCode()), Integer.toHexString(brush.hashCode()), Integer.toHexString(brush.hashCode())));
-//			if (brush == 'R') {
-//				Rectangle r =new Rectangle();
-//				r.setWidth(23);
-//				r.setHeight(23);
-////				r.setStroke(Color.BLACK);
-//				r.setFill(Color.RED);
-//				getChildren().add(r);
-////				Circle A = new Circle();
-////				A.setRadius(15);
-////				A.centerXProperty().bind(this.widthProperty().divide(2));
-////				A.centerYProperty().bind(this.heightProperty().divide(2));
-////				A.setStroke(Color.BLACK);
-////				A.setFill(Color.BLACK);
-////				getChildren().add(A);
-//			}
-//			else if  (brush == 'G'){				
-//				Rectangle g =new Rectangle();
-//				g.setWidth(23);
-//				g.setHeight(23);
-////				r.setStroke(Color.BLACK);
-//				g.setFill(Color.GREEN);
-//				getChildren().add(g);
-//			}
-//			else if  (brush == 'B'){				
-//				Rectangle b =new Rectangle();
-//				b.setWidth(23);
-//				b.setHeight(23);
-////				r.setStroke(Color.BLACK);
-//				b.setFill(Color.BLUE);
-//				getChildren().add(b);
-//			}
+		
+		
+		public void setColor(Color c) {
+			whichColor = c;
+//			this.setStyle("-fx-border-color: red");
+			BackgroundFill fill = new BackgroundFill(whichColor, null, null);
+			Background background = new Background(fill);
+			this.setBackground(background);
 		}
-
+		
 		public void handleMouseClick() {
-			
-//			if (brush == ' ' && whichColor != ' ') {
-				setBrush(whichColor); // Set token in the cell
-
-				// Check game status
-//				if (isWon(whoseTurn)) {
-//					lblStatus.setText(whoseTurn + " won! The game is over");
-//					
-//					whoseTurn = ' '; // Game is over
-//				}
-//				else if (isFull()) {
-//					lblStatus.setText("Draw! The game is over");
-//					whoseTurn = ' '; // Game is over
-//				}
-//				else {
-//					// Change the turn
-//					whoseTurn = (whoseTurn == 'X') ? 'O' : 'X';
-//					// Display whose turn
-//					lblStatus.setText(whoseTurn + "'s turn");
-//				}
-			}
+			setColor(whichColor);
 		}
-//	}
+			
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
