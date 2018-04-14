2d045b276fc1b64e7f324b72ec2f9fcb0d347275
Harsh Jangid <harshjangid1015@gmail.com>
2018-04-13 21:35:30 -0500

    Dictionary GUI app added

---
+++ AdvJava4120/src/cs4120/Dictionary.java
@@ -0,0 +1,14 @@
+package cs4120;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Dictionary extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
