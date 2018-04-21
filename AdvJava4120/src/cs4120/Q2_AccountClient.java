aa72fce40fbb6e995b2f521793046aa921b40cc0
Harsh Jangid <harshjangid1015@gmail.com>
2018-04-20 23:49:30 -0500

    login/ registration form final version add

--- AdvJava4120/src/cs4120/Q2_AccountClient.java
+++ AdvJava4120/src/cs4120/Q2_AccountClient.java
@@ -1,15 +1,10 @@
 package cs4120;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Date;
-
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -32,7 +27,7 @@ public class Q2_AccountClient extends Application {
 		TextField userInput = new TextField();
 		TextField passInput = new TextField();
 		Button register = new Button("Register");
-		Label label = new Label("Hi");
+		Label label = new Label("");
 
 		HBox top = new HBox();
 		top.getChildren().addAll(username, userInput);
@@ -46,120 +41,44 @@ public class Q2_AccountClient extends Application {
 		borderPane.setBottom(bottom);
 
 		int port = 8000;
-		// Socket socket = null;
-
-		System.out.println("Try block start");
 
 		try {
 			socket = new Socket("localhost", port);
 			System.out.println("Client conncetd to server");
 
 			outputToServer = new ObjectOutputStream(socket.getOutputStream());
-			System.out.println("out");
-			// inputFromServer = new ObjectInputStream(socket.getInputStream());
-			System.out.println("in");
 
 		} catch (IOException e1) {
-			System.out.println("catch block start");
 			e1.printStackTrace();
-
 		}
 
 		new Thread(()->{
 			try {
 				while(true) {
-					//					System.out.println("Before Register");
 					register.setOnAction(ev -> {
-						// int port = 8000;
-						// Socket socket = null;
-						//
-						System.out.println("Before Try register");
-						try {
-							System.out.println("In Try register");
-							// socket = new Socket("localhost", port);
-							//
-							// System.out.println("Client conncetd to server");
-
-							// DataInputStream inputFromServer = new
-							// DataInputStream(socket.getInputStream());
-							// ObjectOutputStream outputToServer = new
-							// ObjectOutputStream(socket.getOutputStream());
-
-							// outputToServer = new ObjectOutputStream(socket.getOutputStream());
-							// inputFromServer = new ObjectInputStream(socket.getInputStream());
-							//
-							//							while(true) {
-							//								System.out.println("In Try register: variable");
+						try {						
 							String usr = userInput.getText();
 							String pas = passInput.getText();
-
-							//								System.out.println("In Try register: outputToServer");
 							outputToServer.writeObject(usr);
 							outputToServer.writeObject(pas);
-
-							//								System.out.println("In Try register: Variable clear");
 							userInput.clear();
 							passInput.clear();
 
-							// ObjectInputStream inputFromServer = new
-							// ObjectInputStream(socket.getInputStream());
-							//								System.out.println("In Try register: inputFromServer"+socket.getInputStream().toString());
-
-							//								inputFromServer = new ObjectInputStream(socket.getInputStream());
-
-							//								System.out.println("In Try register: readObject");
-							//								String msg = (String) inputFromServer.readObject();
-							//								label.setText(msg);
-
-							// while(true) {
-							// System.out.print("Enter a radius ");
-							// double radius = input.nextDouble();
-							// System.out.println("radius entered: " + radius);
-							//
-							// outputToServer.writeDouble(radius);
-							//
-							// double area = inputFromServer.readDouble();
-							//
-							// System.out.println("Area received: " + area);
-							//
-							// }
-							//							}
-
 							inputFromServer = new ObjectInputStream(socket.getInputStream());
-							System.out.println("In Try register: readObject");
 							String msg = (String) inputFromServer.readObject();
-							System.out.println(msg);
-//							Platform.runLater(() ->
-//							label.setText(msg));
 							label.setText(msg);
 
 						} catch (IOException ex) {
-							System.out.println("In Try register: catch");
 							ex.printStackTrace();
 						} catch (ClassNotFoundException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					});
-
-					//					ta.appendText("Server started at " + new Date() + '\n'));
-					//					label.setText(msg);
-					//					String message = (String)inputFromServer.readObject();					
-					//					ta.appendText(message);
 				}
 			}catch(Exception ex) {
 
 			}
 		}).start();
-
-		new Thread(()->{
-
-
-
-		}).start();
-
-
-		System.out.println("opening window");
 
 		Scene scene = new Scene(borderPane, 400, 200);
 		primaryStage.setTitle("Register Account");
--- AdvJava4120/src/cs4120/Q2_AccountServer.java
+++ AdvJava4120/src/cs4120/Q2_AccountServer.java
@@ -1,7 +1,5 @@
 package cs4120;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -12,11 +10,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-
-import cs4120.Q3_ChatRoomServer.HandleClientTasks;
-import javafx.application.Platform;
 
 public class Q2_AccountServer {
 
@@ -31,26 +24,15 @@ public class Q2_AccountServer {
 
 		new Thread(() -> {
 			try {
-
 				int clientID = 0;
 				int port = 8000;
 				ServerSocket serverSocket = new ServerSocket(port);
-				// Platform.runLater(() ->
-				// ta.appendText("Server started at " + new Date() + '\n'));
 				System.out.println("Networking setup. Waiting for client.");
 
 				while (true) {
-					System.out.println("inside while");
-					// String userName = "";
 					Socket socket = serverSocket.accept();
-					// ta.appendText("Client " +clientID+ " connected"+ '\n');
 					System.out.println("Server connected to Client " + clientID + " (localhost at 127.0.0.1)");
 					HandleClientTaskAcc task = new HandleClientTaskAcc(socket, clientID);
-					System.out.println("in new thread try");
-
-					// ObjectOutputStream outputToClient = new
-					// ObjectOutputStream(socket.getOutputStream());
-					// clientList.put(socket, outputToClient);
 
 					Thread thread = new Thread(task);
 					thread.start();
@@ -60,22 +42,6 @@ public class Q2_AccountServer {
 				e.printStackTrace();
 			}
 		}).start();
-		// }});
-
-		// // Create a statement
-		// Statement statement = connection.createStatement();
-		//
-		// // Execute a statement
-		// ResultSet resultSet = statement.executeQuery
-		// ("select word, definition from entry where word "
-		// + " = 'hello'");
-		//// ("select firstName, mi, lastName from Student where lastName "
-		//// + " = 'Smith'");
-		//
-		// // Iterate through the result and print the student names
-		// while (resultSet.next())
-		// System.out.println(resultSet.getString(1) + "\t" +
-		// resultSet.getString(2));
 
 		// Close the connection
 		connection.close();
@@ -97,65 +63,38 @@ class HandleClientTaskAcc implements Runnable {
 		try {
 			ObjectInputStream inputFromClient = new ObjectInputStream(socket.getInputStream());
 
-			 while(true) {
-			String message1 = (String) inputFromClient.readObject();
-			String message2 = (String) inputFromClient.readObject();
-			// sendToAll(message, id);
-			// System.out.println(message);
+			while(true) {
+				String message1 = (String) inputFromClient.readObject();
+				String message2 = (String) inputFromClient.readObject();
+				Class.forName("com.mysql.jdbc.Driver");
 
-//			 }
-			Class.forName("com.mysql.jdbc.Driver");
-			// System.out.println("Driver loaded.");
+				// Establish a connection
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dictionary", "root", "root");
 
-			// Establish a connection
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dictionary", "root", "root");
-			// System.out.println("Connection established.");
+				ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());;
+				if (loginValidation(message1, message2)) {
+					outputToClient.writeObject("Username already exist");
+					System.out.println("Client " + id + ": " + "Account " + "'"+message1+"'" + " already exists.");
+				} else {
+					PreparedStatement preparedStatement = connection
+							.prepareStatement("insert into midterm.account (username, password) " + "values (?, ?)");
+					preparedStatement.setString(1, message1);
+					preparedStatement.setString(2, message2);
+					preparedStatement.executeUpdate();
 
-			// Statement statement = connection.createStatement();
-			ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());
+					outputToClient.writeObject("Account created");
+					System.out.println(
+							"Client " + id + ": " + "Account " +"'"+ message1+"'" + " with password " + "'"+message2+"'" + " created.");
+				}
 
-			System.out.println("loginValidation");
-			if (loginValidation(message1, message2)) {
-				System.out.println("if");
-				outputToClient.writeObject("Username already exist");
-				System.out.println("Client " + id + ": " + "Account " + message1 + " already exists.");
-			} else {
-				System.out.println("else");
-				PreparedStatement preparedStatement = connection
-						.prepareStatement("insert into midterm.account (username, password) " + "values (?, ?)");
-				preparedStatement.setString(1, message1);
-				preparedStatement.setString(2, message2);
-				preparedStatement.executeUpdate();
-
-				outputToClient.writeObject("Account created");
-				System.out.println(
-						"Client " + id + ": " + "Account " + message1 + " with password " + message2 + " created.");
+				connection.close();
 			}
-
-			// PreparedStatement preparedStatement = connection.prepareStatement
-			// ("insert into midterm.account (username, password) " +"values (?, ?)");
-			// preparedStatement.setString(1, message1);
-			// preparedStatement.setString(2, message2);
-			// preparedStatement.executeUpdate();
-			//
-			// ObjectOutputStream outputToClient = new
-			// ObjectOutputStream(socket.getOutputStream());
-			// outputToClient.writeObject("Account created");
-
-			connection.close();
-
-			// System.out.println("Client "+id+": "+"Account "+message1+" with password
-			// "+message2+" created.");
-
-			// Statement statement = connection.createStatement();
-			 }
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -163,19 +102,12 @@ class HandleClientTaskAcc implements Runnable {
 	public boolean loginValidation(String userName, String password) throws SQLException {
 		PreparedStatement preparedStatement;
 		ResultSet resultSet;
-		String query = "select * from midterm.account where username = ? and password = ?";
-		System.out.println("loginValidation before try");
+		String query = "select * from midterm.account where username = ?";
 		try {
-			System.out.println("loginValidation in try");
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dictionary", "root", "root");
 			preparedStatement = connection.prepareStatement(query);
-			
-			System.out.println("loginValidation in try preparedStatement");
 			preparedStatement.setString(1, userName);
-			preparedStatement.setString(2, password);
 			resultSet = preparedStatement.executeQuery();
-			
-			System.out.println("loginValidation in try resultSet");
 			if (resultSet.next()) {
 				return true;
 			} else {
